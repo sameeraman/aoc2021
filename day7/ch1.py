3ebc3d5bc52b34c18563bad7866a98ddc7975dfd
Sameera Perera <sameera.man@gmail.com>
2021-12-07 14:49:23 +0800

    adding day7 answers

---
+++ day7/ch1.py
@@ -0,0 +1,18 @@
+with open("input.txt") as f:
+    line = f.readline()
+
+crab_positions = [int(crab) for crab in line.split(',')]
+positions = range(max(crab_positions))
+costs = [0]*len(positions)
+
+for position in positions:
+    position_cost = 0
+    for crab_position in crab_positions:
+        cost = max(crab_position,position)-min(crab_position,position)
+        position_cost += cost
+    costs[position] = position_cost
+
+print("Answer: Index =", costs.index(min(costs)), ", Fuel= ", min(costs))
+
+
+
